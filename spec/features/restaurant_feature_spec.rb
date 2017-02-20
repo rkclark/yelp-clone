7d3ec001718009b7531fffbd7a31a13d6721face
Sergey Drozdov <srgdzv@gmail.com>
2017-02-20 16:18:38 +0000

    Written a test for when a restaurant is added and created a restaurant model

--- spec/features/restaurant_feature_spec.rb
+++ spec/features/restaurant_feature_spec.rb
@@ -8,4 +8,17 @@ feature 'restaurant' do
       expect(page).to have_link'Add a restaurant'
     end
   end
+
+  context ' restaurants have been added' do
+    before do
+      Restaurant.create(name: 'KFC')
+    end
+
+    scenario 'display restaurants' do
+      visit '/restaurants' do
+        expect(page).to have_contetnt('KFC')
+        expect(page).not_to have_content('No restaurants yet')
+      end
+    end
+  end
 end
