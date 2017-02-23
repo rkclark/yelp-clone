15382aa1587c665a9076ad071217f7190cbdca77
Rick Clark <rkingsleyclark@gmail.com>
2017-02-23 11:59:39 +0000

    Refactored building of review with a user into a module

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,14 +1,10 @@
 class Restaurant < ApplicationRecord
   belongs_to :user
-  has_many :reviews, dependent: :destroy
+  has_many :reviews,
+           -> { extending WithUserAssociationExtension },
+           dependent: :destroy
 
   validates :name, length: { minimum: 3 }, uniqueness: true
-
-  def build_review(review_params, current_user)
-    review = reviews.new(review_params)
-    review.user = current_user
-    review
-  end
 
   def belongs_to_currrent_user?(current_user)
     user_id == current_user.id
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -11,4 +11,21 @@ describe Review, type: :model do
     review = Review.new(rating: 10)
     expect(review).to have(1).error_on(:rating)
   end
+
+  describe 'build_with_user' do
+
+    let(:user) { User.create email: 'test@test.com' }
+    let(:restaurant) { Restaurant.create name: 'Test' }
+    let(:review_params) { {rating: 5, thoughts: 'yum'} }
+
+    subject(:review) { restaurant.reviews.build_with_user(review_params, user) }
+
+    it 'builds a review' do
+      expect(review).to be_a Review
+    end
+
+    it 'builds a review associated with the specified user' do
+      expect(review.user).to eq user
+    end
+  end
 end
