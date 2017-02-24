79845ea41d518de312374d1a2708848e520812b2
Rick Clark <rkingsleyclark@gmail.com>
2017-02-24 09:26:29 +0000

    Added Paperclip and restaurant image upload

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -6,6 +6,9 @@ class Restaurant < ApplicationRecord
 
   validates :name, length: { minimum: 3 }, uniqueness: true
 
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
   def belongs_to_currrent_user?(current_user)
     user_id == current_user.id
   end
